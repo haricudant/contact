9f5feb26d7edcec87f1a3c84feacb477684ad656
Hari Narayanan <hnarayanan5@gmail.com>
2020-07-30 03:28:46 +0100

    df

--- contact/settings.py
+++ contact/settings.py
@@ -123,4 +123,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 django_heroku.settings(locals())
-ALLOWED_HOSTS =  ['https: // hericudantapi.herokuapp.com/']
+ALLOWED_HOSTS = ['https://hericudantapi.herokuapp.com/']
