c4df469634f1c0ec87ee85efe0116217e9e0d0de
Hari Narayanan <hnarayanan5@gmail.com>
2020-07-30 03:37:50 +0100

    df

--- contact/settings.py
+++ contact/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+import dj_database_url
 import os
 import django_heroku
 
@@ -117,10 +118,19 @@ USE_L10N = True
 USE_TZ = True
 
 
+prod_db = dj_database_url.config(conn_max_age=500)
+
+DATABASES['default'].update(prod_db)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 django_heroku.settings(locals())
 ALLOWED_HOSTS = ['https://hericudantapi.herokuapp.com/']
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
