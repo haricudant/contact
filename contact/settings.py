1272558c4053385b15c9d5ee22f6e62890f7ea28
Hari Narayanan <hnarayanan5@gmail.com>
2020-07-30 17:54:50 +0100

    Inital Commit

--- contact/settings.py
+++ contact/settings.py
@@ -146,4 +146,4 @@ ALLOWED_HOSTS = ['harimass.herokuapp.com']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 prod_db = dj_database_url.config(conn_max_age=500)
-DATABASES['Default'].update(prod_db)
+DATABASES['default'].update(prod_db)
