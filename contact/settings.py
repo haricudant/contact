b69b541a3754a68e6b3a726842e88f57a46db9d7
Hari Narayanan <hnarayanan5@gmail.com>
2020-07-30 17:25:07 +0100

    Inital Commit

--- contact/settings.py
+++ contact/settings.py
@@ -79,8 +79,7 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-prod_db = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+
 
 DATABASES = {
     'default': {
