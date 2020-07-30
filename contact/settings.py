11fd0c5f28c892345962eadb38392adb224609ed
Hari Narayanan <hnarayanan5@gmail.com>
2020-07-30 17:48:11 +0100

    Inital Commit

--- contact/settings.py
+++ contact/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 
+
 import dj_database_url
 import os
 import django_heroku
@@ -81,11 +82,13 @@ TEMPLATES = [
 
 
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+   
 }
 
 
@@ -141,3 +144,6 @@ ALLOWED_HOSTS = ['harimass.herokuapp.com']
 # db_from_env = dj_database_url.config(conn_max_age=500)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['Contact'].update(prod_db)
